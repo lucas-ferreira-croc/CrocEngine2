1641bb9a95e3ffd36875ab5899e9cca9a9b31ab5
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2023-09-26 22:58:28 -0300

    implementing log wrapper uppon spdlog

--- Croc/src/Croc.h
+++ Croc/src/Croc.h
@@ -2,6 +2,7 @@
 // For use by applications only
 
 #include "Croc/Application.h"
+#include "Croc/Log.h"
 
 // ------ Entry Point -------------------------------------
 #include "Croc/EntryPoint.h"
--- Croc/src/Croc/EntryPoint.h
+++ Croc/src/Croc/EntryPoint.h
@@ -6,6 +6,12 @@ extern Croc::Application* Croc::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Croc::Log::Init();
+	CROC_CORE_WARN("Initialized Log!");
+
+	int a = 5;
+	CROC_INFO("Initialized Log! var = {0}", a);
+
 	auto app = Croc::CreateApplication();
 	app->Run();
 	delete app;
